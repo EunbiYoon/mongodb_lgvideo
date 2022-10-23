c7fe637651b518f1205c8c7ff50847adba7c473f
Software Developer <yinyinbei0717@gmail.com>
2022-10-23 16:56:21 -0500

    asdfa

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def create_app():
         ]
         return render_template("index.html", entries=entries_with_date)
     if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000")
+        port = int(os.environ.get("PORT", 5000))
         app.run(host="0.0.0.0", port=port)
     return app
 
