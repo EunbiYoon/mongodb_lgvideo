1eb4389b40769141c791e49b7bacc807dcaef8c4
Software Developer <yinyinbei0717@gmail.com>
2022-10-23 14:22:23 -0500

    fgfgsdfa

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 def create_app():
     app=Flask(__name__)
-    client=MongoClient("MONGODB_URI")
+    client=MongoClient(os.environ.get("MONGODB_URI"))
     app.db=client.voc
 
     @app.route("/", methods=["GET","POST"])
